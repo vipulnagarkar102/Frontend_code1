cbb32726700a53146d8ad5d0223cfb9ba52db7cb
Curiouspods <Curiouspods@gmail.com>
2025-03-27 19:22:42 +0530

    layout.tsx changed

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar/>
-        <div className="h-screen bg-[#FFFFFF] flex items-center justify-center text-black font-bold text-[24px]">
+        <div className="h-screen bg-[#FFFFFF] flex text-black font-bold text-[24px]">
         {children}
         </div>
         <Footer/>
