5df252a8e5381f8006516fd9a5518cb15f26893b
Curiouspods <Curiouspods@gmail.com>
2025-03-29 02:46:55 +0530

    Testimonials component added

---
+++ frontend/src/app/_components/Home/RotateText.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RotateText = () => {
+  return (
+    <p className="hidden lg:block top-0 -ml-44 font-poppins text-[32px] text-center font-semibold text-white bg-[#00A5CF] transform -rotate-90 w-[400px]">
+          START FOR FREE
+    </p>
+  )
+}
+
+export default RotateText
