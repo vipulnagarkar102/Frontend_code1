b02287f4a2a15f4cb35908e1cca6bb0bb4a54c7e
Curiouspods <Curiouspods@gmail.com>
2025-04-02 16:16:45 +0530

    customer dashboard  updated

--- frontend/src/app/customer-dashboard/sidebar.tsx
+++ frontend/src/app/customer-dashboard/sidebar.tsx
@@ -1,25 +1,34 @@
 'use client';
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   const menuItems = [
-    { name: 'Dashboard', path: '/customer-dashboard' },
-    { name: 'Profile', path: '/customer-dashboard/my-profile' },
-    { name: 'Enrolled Courses', path: '/customer-dashboard/my-videos' },
-    { name: 'Orders', path: '/customer-dashboard/my-orders' },
-    { name: 'Reviews', path: '/customer-dashboard/my-reviews' },
-    { name: 'Pay Per Code', path: '/customer-dashboard/my-reviews' },
-    { name: 'Buy Commercial License', path: '/customer-dashboard/my-reviews' },
-    { name: 'Consult Us', path: '/customer-dashboard/my-reviews' },
-    { name: 'Settings', path: '/customer-dashboard/settings' },
+    { id: 1, name: 'Dashboard', path: '/customer-dashboard' },
+    { id: 2, name: 'Profile', path: '/customer-dashboard/my-profile' },
+    { id: 3, name: 'Enrolled Courses', path: '/customer-dashboard/my-videos' },
+    { id: 4, name: 'Orders', path: '/customer-dashboard/my-orders' },
+    { id: 5, name: 'Reviews', path: '/customer-dashboard/my-reviews' },
+    { id: 6, name: 'Pay Per Code', path: '/customer-dashboard/my-pay-per-code' },
+    { id: 7, name: 'Buy Commercial License', path: '/customer-dashboard/my-commercial-license' },
+    { id: 8, name: 'Consult Us', path: '/customer-dashboard/consult-us' },
+    { id: 9, name: 'Settings', path: '/customer-dashboard/settings' },
   ];
 
   return (
     <div className="text-lg font-normal w-64 bg-[#003F5C] text-white p-5 hidden md:block h-screen fixed top-26 left-0">
       <ul>
-        {menuItems.map((item, index) => (
-          <li key={index} className={`hover:bg-[#064867] cursor-pointer hover:text-[#ffffff] hover:rounded-lg p-3 ${index === 0 ? 'bg-white font-semibold text-lg text-[#003F5C] rounded-lg' : ''}`}>
+        {menuItems.map((item) => (
+          <li
+            key={item.id}
+            id={`menu-item-${item.id}`}
+            className={`cursor-pointer hover:bg-[#064867] hover:text-[#ffffff] hover:rounded-lg p-3 ${
+              pathname === item.path ? 'bg-white font-semibold text-lg text-[#003F5C] rounded-lg' : ''
+            }`}
+          >
             <Link href={item.path}>{item.name}</Link>
           </li>
         ))}
