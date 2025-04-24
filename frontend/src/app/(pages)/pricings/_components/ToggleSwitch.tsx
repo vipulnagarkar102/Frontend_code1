66d836f5c6350f4aade1ee7793c67faf5392c09b
Curiouspods <Curiouspods@gmail.com>
2025-04-24 16:27:37 +0530

    our offering page modified

--- frontend/src/app/(pages)/pricings/_components/ToggleSwitch.tsx
+++ frontend/src/app/(pages)/pricings/_components/ToggleSwitch.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ToggleSwitch = ({ checked, onChange }: { checked: boolean, onChange: () => void }) => (
   <label className="relative inline-flex items-center cursor-pointer">
-    <input type="checkbox" checked={checked} onChange={onChange} className="sr-only peer" />
+    <input title='checkbox' type="checkbox" checked={checked} onChange={onChange} className="sr-only peer" />
     <div className="w-11 h-6 bg-gray-300 peer-focus:outline-none peer-checked:bg-[#00A5CF] rounded-full peer peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
   </label>
 );
