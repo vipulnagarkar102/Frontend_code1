9b840fa98bc357309a26ab2eee157a26fe48d422
vipulnagarkar102 <vipulnagarkar102@gmail.com>
2025-05-03 12:24:33 +0530

    updateddd

--- frontend/src/app/auth/success/page.tsx
+++ frontend/src/app/auth/success/page.tsx
@@ -1,13 +1,12 @@
 'use client';
 export const dynamic = 'force-dynamic';
 
-
 import { Suspense } from 'react';
 import AuthSuccessPage from './AuthSuccessPage';
 
 export default function Page() {
   return (
-    <Suspense fallback={<div>Loading</div>}>
+    <Suspense fallback={<div>Loading...</div>}>
       <AuthSuccessPage />
     </Suspense>
   );
