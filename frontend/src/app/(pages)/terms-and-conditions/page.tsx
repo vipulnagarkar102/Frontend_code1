ae8abff20cd686a3a8140a412ba5a7cf3f18af8c
vipulnagarkar102 <vipulnagarkar102@gmail.com>
2025-05-08 10:37:59 +0530

    terms and condition

--- frontend/src/app/(pages)/terms-and-conditions/page.tsx
+++ frontend/src/app/(pages)/terms-and-conditions/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from 'react';
-import { ArrowDown } from 'lucide-react';
+import { ArrowDown, ChevronRight } from 'lucide-react';
 import Card from './_components/TermsCard'
 import termsData from '@/app/data/terms.json';
 import Footer from '@/app/_components/Footer';
@@ -15,8 +15,7 @@ export default function TermsAndConditionsPage() {
         {/* Header Section */}
         <div className="mb-8 [@media(min-width:1750px)]:mb-14 text-center space-y-1">
           <h1 className="text-[50px] [@media(min-width:1750px)]:text-[64px] font-poppins font-semibold mb-4">Terms and Conditions</h1>
-          {/* <p className="text-[22px] [@media(min-width:1750px)]:text-[28px] font-normal">Please read this document carefully.</p> */}
-          <p className="text-[22px] [@media(min-width:1750px)]:text-[28px] font-normal">Last updated  1st April 2025</p>
+          <p className="text-[22px] [@media(min-width:1750px)]:text-[28px] font-normal">Last updated 01 April 2025</p>
         </div>
         
         {/* Cards Grid */}
@@ -26,15 +25,16 @@ export default function TermsAndConditionsPage() {
               href={`/terms-and-conditions/${item.id}`}
               key={item.id}>
               <Card 
-                icon={item.icon ?? "DefaultIcon"}
+                
+                icon={item.icon}
                 title={item.title}
               />
             </Link>
           ))}
         </div>
         
-        {/* Downloads Section */}
-        <div className="mt-8 md:mt-12">
+         {/* Downloads Section */}
+         <div className="mt-8 md:mt-12">
           <h2 className="text-[40px] [@media(min-width:1750px)]:text-[56px] font-semibold font-poppins mb-4">Downloads</h2>
           <div className="flex flex-col space-y-2">
             <a href="/Copyright Takedown Notice Form.docx" download className="font-normal text-[18px] [@media(min-width:1750px)]:text-[22px] hover:underline flex items-center">
